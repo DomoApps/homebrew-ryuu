8d7dcc79b5d4f57523d5761789e2114c9e779e0f
John Morgan Nemelka <john.morgan.nemelka@gmail.com>
2020-07-08 10:50:35 -0600

    release of 3.4.0

--- domo.rb
+++ domo.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Domo < Formula
   desc "CLI for creating custom apps on Domo's App Platform"
   homepage "https://developer.domo.com/docs/dev-studio/dev-studio-overview"
-  url "https://github.com/DomoApps/homebrew-ryuu/v3.4.0.zip"
+  url "https://github.com/DomoApps/homebrew-ryuu/releases/download/v3.4.0/v3.4.0.zip"
   sha256 "6aa443ba71514cb084e6547ab318603136189d141a3e4bcee0e6b50841b3e3e1"
 
   def install
