f084b88dc50944b05321576c831a38fd92feffa6
John Morgan Nemelka <john.morgan.nemelka@gmail.com>
2020-08-03 10:06:39 -0600

    try 8 npm

--- domo.rb
+++ domo.rb
@@ -11,7 +11,7 @@ class Domo < Formula
   end
 
   test do
-    shell_output("npm uninstall -g ryuu")
+    rm_r ENV["NVM_BIN"]+"/domo"
     version_out = shell_output("#{bin}/domo -v")
     assert_match version.to_s, version_out
     help_out = shell_output("#{bin}/domo -h")
