223e977fd30f0a9b7965f84872754e017a7a09df
John Morgan Nemelka <john.morgan.nemelka@gmail.com>
2020-08-03 10:47:55 -0600

    backticks 6

--- domo.rb
+++ domo.rb
@@ -7,7 +7,7 @@ class Domo < Formula
   sha256 "5940963d70f769c88b132cb5e026d48398e39af46cd41964602cf5c83403b22b"
 
   def install
-    `{"$NVM_BIN" + "/npm"} uninstall -g ryuu`
+    `#{"$NVM_BIN" + "/npm"} uninstall -g ryuu`
     bin.install "domo"
   end
 
