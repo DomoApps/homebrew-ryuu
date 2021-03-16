481fb08e121a0f728c9e5284b5fc81de9e2f8fc8
John Morgan Nemelka <john.morgan.nemelka@gmail.com>
2021-03-16 14:52:38 -0600

    4.1.1 formula update

--- domo.rb
+++ domo.rb
@@ -3,15 +3,15 @@ require 'formula'
 class Domo < Formula
   desc "CLI for creating custom apps on Domo's App Platform"
   homepage "https://developer.domo.com/docs/dev-studio/dev-studio-overview"
-  url "https://github.com/DomoApps/homebrew-ryuu/releases/download/v4.1.0/domo.zip"
-  sha256 "d704ab6cbf8a0c620e6178c8f922e292895648ac71f162be8358dda1da939e8b"
+  url "https://github.com/DomoApps/homebrew-ryuu/releases/download/v4.1.1/domo.zip"
+  sha256 "94d1841bf902e0243286f6ab828c3d32b936bae24e3314de4712f7cb17f36f4f"
 
   def install 
     bin.install "domo"
   end
 
   test do
-    rm "/Users/johnnemelka/.nvm/versions/node/v12.16.1/bin/domo"
+    rm "~/.nvm/versions/node/v12.16.1/bin/domo"
     version_out = shell_output("#{bin}/domo -v")
     assert_match version.to_s, version_out
     help_out = shell_output("#{bin}/domo -h")
