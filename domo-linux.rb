5a7a22f65a765e0fcabfe80b8713023be8d82302
John Morgan Nemelka <john.morgan.nemelka@gmail.com>
2021-03-17 11:56:47 -0600

    linux brew release for 4.1.1

---
+++ domo-linux.rb
@@ -0,0 +1,22 @@
+require 'formula'
+
+class Domo < Formula
+  desc "CLI for creating custom apps on Domo's App Platform"
+  homepage "https://developer.domo.com/docs/dev-studio/dev-studio-overview"
+  url "https://github.com/DomoApps/homebrew-ryuu/releases/download/v4.1.1/domo-linux.zip"
+  sha256 "58d0c00008d31a56f1bccb4d9e74d993db5f685c40abca84d2f351c414acf601"
+
+  def install 
+    bin.install "domo"
+  end
+
+  test do
+    rm "~/.nvm/versions/node/v12.16.1/bin/domo"
+    version_out = shell_output("#{bin}/domo -v")
+    assert_match version.to_s, version_out
+    help_out = shell_output("#{bin}/domo -h")
+    assert_match "Usage: domo [options] [command]", help_out
+    assert_match "", help_out
+    assert_match "Options:", help_out
+  end
+end
