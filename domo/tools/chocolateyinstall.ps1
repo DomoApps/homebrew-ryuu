14bef5d2aef1f6a1c0e6a1ab280b40a0ba7be0de
Caledrith <7317036+Caledrith@users.noreply.github.com>
2025-01-23 12:25:20 -0700

    Adding chocolatey package here

---
+++ domo/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/DomoApps/homebrew-ryuu/releases/download/v4.4.7/domo.exe.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+
+  softwareName  = 'Domo*'
+
+  checksum      = 'dfd8f62b36d59d2488eb8aedde554573167c4d89307c79ea99c1d1e57545b7c8'
+  checksumType  = 'sha256'
+
+}
+
+Install-ChocolateyZipPackage @packageArgs
